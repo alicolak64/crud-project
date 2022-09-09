2e4b3280e3997e395f6697e8fd92dfe9bdd4eeda
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-09 17:52:48 +0300

    Fixed App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <br />
+      <br />
+      <br />
+
+      <img src="http://anlami-nedir.com/resimler/2/crud.jpg" alt="" height={"300px"} />
+      <br />
+      <br />
+      <br />
+      <h1>Crud Project</h1>
     </div>
   );
 }
