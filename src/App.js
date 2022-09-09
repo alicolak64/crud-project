92f048847fe8a7a1bdc7dbd987f003a009c2f09a
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-09 18:03:02 +0300

    Added necessary components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import './App.css';
+import EmployeeList from './Components/EmployeeList';
 
 function App() {
   return (
     <div className="App">
 
-      <br />
-      <br />
-      <br />
-
-      <img src="http://anlami-nedir.com/resimler/2/crud.jpg" alt="" height={"300px"} />
-      <br />
-      <br />
-      <br />
-      <h1>Crud Project</h1>
+      
+      <EmployeeList />
     </div>
   );
 }
---
+++ src/Components/Employee.js
@@ -0,0 +1,14 @@
+
+
+const Employee = () => {
+    return (
+        <ul>
+            <li>Employee1</li>
+            <li>Employee2</li>
+            <li>Employee3</li>
+            <li>Employee4</li>
+        </ul>
+    )
+}
+
+export default Employee
