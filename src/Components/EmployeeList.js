dd2573944912d329cab40781b4f1418187e421e4
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-12 16:28:24 +0300

    Added Table title and modal

--- src/Components/EmployeeList.js
+++ src/Components/EmployeeList.js
@@ -1,30 +1,78 @@
-import { useContext } from 'react'
+import { useState , useContext } from 'react'
+import { Button, Modal } from 'react-bootstrap';
 
 import { EmployeeContext } from '../Contexts/EmployeeContext'
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import Employee from './Employee';
+import AddEmployee from './AddEmployee';
 
 const EmployeeList = () => {
 
-    
-    const {employees} = useContext (EmployeeContext)
+
+    const { employees } = useContext(EmployeeContext)
+    const [show, setShow] = useState(false)
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
 
-        <table className="table table-striped table-hover">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Email</th>
-                    <th>Address</th>
-                    <th>Phone</th>
-                    <th>Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <Employee employees={employees}/>
-            </tbody>
-        </table>
+        <>
+
+            <div className="table-title">
+                <div className="row">
+                    <div className="col-sm-6">
+                        <h2>Manage <b>Employees</b></h2>
+                    </div>
+                    <div className="col-sm-6">
+                        <Button
+                            className="btn btn-success text-white"
+                            data-toggle="modal"
+                            onClick={handleShow}
+                            >
+                            <i className="material-icons">&#xE147;</i>
+                            <span>Add New Employee</span>
+                        </Button>
+                    </div>
+                </div>
+            </div>
+
+            <table className="table table-striped table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Address</th>
+                        <th>Phone</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <Employee employees={employees} />
+                </tbody>
+            </table>
+
+            <Modal show={show} onHide = {handleClose}>
+                <Modal.Header className='modal-header'closeButton >
+                    <Modal.Title>Add Employee</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <AddEmployee />
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button 
+                    variant="secondary"
+                    onClick={handleClose}
+                    >
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+
+        </>
     )
 }
 
