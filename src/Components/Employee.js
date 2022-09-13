64496247767268e3c18740010b6993362fe26cfa
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-13 13:24:37 +0300

    Fixed Employee Component

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -2,50 +2,50 @@ import { useContext } from 'react';
 
 import { EmployeeContext } from '../Contexts/EmployeeContext'
 
-const Employee = ({ employees }) => {
+const Employee = ({ employee }) => {
 
     const { deleteEmployee } = useContext(EmployeeContext)
 
     return (
 
         <>
-            {
-                employees.map((employee) => (
-                    <tr key={employee.id}>
-                        <td>{employee.name}</td>
-                        <td>{employee.email}</td>
-                        <td>{employee.address}</td>
-                        <td>{employee.phone}</td>
-                        <td>
-                            <button
-                                className="btn text-warning btn-act"
-                                data-toggle="modal"
-                            >
-                                <i
-                                    className="material-icons"
-                                    data-toggle="tooltip"
-                                    title="Edit"
-                                >
-                                    &#xE254;
-                                </i>
-                            </button>
-                            <button
-                                className="btn text-danger btn-act"
-                                data-toggle="modal"
-                                onClick = { () => deleteEmployee (employee.id) }
-                                >
-                                <i
-                                    className="material-icons"
-                                    data-toggle="tooltip"
-                                    title="Delete"
-                                >
-                                    &#xE872;
-                                </i>
-                            </button>
-                        </td>
-                    </tr>
-                ))
-            }
+
+
+
+            <td>{employee.name}</td>
+            <td>{employee.email}</td>
+            <td>{employee.address}</td>
+            <td>{employee.phone}</td>
+            <td>
+                <button
+                    className="btn text-warning btn-act"
+                    data-toggle="modal"
+                >
+                    <i
+                        className="material-icons"
+                        data-toggle="tooltip"
+                        title="Edit"
+                    >
+                        &#xE254;
+                    </i>
+                </button>
+                <button
+                    className="btn text-danger btn-act"
+                    data-toggle="modal"
+                    onClick={() => deleteEmployee(employee.id)}
+                >
+                    <i
+                        className="material-icons"
+                        data-toggle="tooltip"
+                        title="Delete"
+                    >
+                        &#xE872;
+                    </i>
+                </button>
+            </td>
+
+
+
         </>
 
     )
