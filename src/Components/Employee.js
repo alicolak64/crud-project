d5ecec0af06e118e1e7103e182abf9714cb3cbf1
Ali Çolak <20190808064@ogr.akdeniz.edu.tr>
2022-09-13 13:03:55 +0300

    Added deleteButton to Employee Component

--- src/Components/Employee.js
+++ src/Components/Employee.js
@@ -1,23 +1,52 @@
-const Employee = ({employees}) => {
+import { useContext } from 'react';
+
+import { EmployeeContext } from '../Contexts/EmployeeContext'
+
+const Employee = ({ employees }) => {
+
+    const { deleteEmployee } = useContext(EmployeeContext)
 
     return (
 
         <>
-        {
-            employees.map((employee) => (
-            <tr key={employee.id}>
-                <td>{employee.name}</td>
-                <td>{employee.email}</td>
-                <td>{employee.address}</td>
-                <td>{employee.phone}</td>
-                <td>
-                    <a href="#editEmployeeModal" className="edit" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Edit">&#xE254;</i></a>
-                    <a href="#deleteEmployeeModal" className="delete" data-toggle="modal"><i className="material-icons" data-toggle="tooltip" title="Delete">&#xE872;</i></a>
-                </td>
-            </tr>
-            ))
-        }
-         </>
+            {
+                employees.map((employee) => (
+                    <tr key={employee.id}>
+                        <td>{employee.name}</td>
+                        <td>{employee.email}</td>
+                        <td>{employee.address}</td>
+                        <td>{employee.phone}</td>
+                        <td>
+                            <button
+                                className="btn text-warning btn-act"
+                                data-toggle="modal"
+                            >
+                                <i
+                                    className="material-icons"
+                                    data-toggle="tooltip"
+                                    title="Edit"
+                                >
+                                    &#xE254;
+                                </i>
+                            </button>
+                            <button
+                                className="btn text-danger btn-act"
+                                data-toggle="modal"
+                                onClick = { () => deleteEmployee (employee.id) }
+                                >
+                                <i
+                                    className="material-icons"
+                                    data-toggle="tooltip"
+                                    title="Delete"
+                                >
+                                    &#xE872;
+                                </i>
+                            </button>
+                        </td>
+                    </tr>
+                ))
+            }
+        </>
 
     )
 }
